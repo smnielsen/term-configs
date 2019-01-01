9a252aa1b93cf10f0bb8fa7f9f7e3ecbc70f69ae
Simon Nielsen <simonmtnielsen@gmail.com>
2019-01-02 00:11:44 +0100

    Updated init script

--- init-new-mac.sh
+++ init-new-mac.sh
@@ -2,14 +2,21 @@
 start=$(date +%s)
 log() {
   local t=$1
-  local end=$(date +%s)
-  printf "#\033[34;1m $t:\033[0m$((($end-$start)%60))s\n"
+  printf "  \033[37;1m$t\033[0m\n"
+}
+
+STEP_COUNT=0
+step() {
+  local t=$1
+  ((STEP_COUNT++))
+  printf "# \033[34;1mStep ${STEP_COUNT}: $t\033[0m\n"
+  start=$(date +%s)
 }
 
 success() {
   local t=$1
   local end=$(date +%s)
-  printf "\r   \033[32;1m $t:\033[0m $((($end-$start)%60))s\n"
+  printf "  \033[32;1mDone ${STEP_COUNT}: $t:\033[0m $((($end-$start)%60))s\n"
 }
 
 echoAll() {
@@ -100,42 +107,54 @@ YARN_GLOBAL=(
 ## Not avaliable
 MANUAL=(
   "Outlook;https://webmail.netlight.com"
+  "Docker;https://hub.docker.com/editions/community/docker-ce-desktop-mac"
 )
+
+step "Installation of a new Mac OS X Computer"
+log "Setup is done with no promise of success"
+log "Continuing is done on your own risk. :)"
+log "¯\_(ツ)_/¯"
+while true; do
+  read -p "Do you wish to continue (y/n)? " yn
+  case $yn in
+    [Yy]* ) echo "Continuing..."; break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
 
 ###############################
 ## PACKAGE EVALS
-log "Verifying .zshrc..."
+step "Verifying oh-my-zsh..."
 if [ ! -f ${HOME}/.zshrc ]; then
   log "Missing .zshrc!"
   log "Please use oh-my-zsh for this suite"
   log "Installation guide: https://github.com/robbyrussell/oh-my-zsh"
   exit 1
 fi
-success "oh-my-zsh is setup correctly"
+success "Found oh-my-zsh"
 
-log "Initialising new Mac"
 PREV_DIR=$(pwd)
 DIRNAME=$(dirname $0)
 cd ## Start in HOME dir
 
-log "Installing Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-success "Installed Homebrew"
-
-log "Setting up oh-my-zsh"
-echo "## Custom config from: https://github.com/smnielsen/config" >> .zshrc
-echo "export SMN_CONFIG_DIR=${DIRNAME}" >> .zshrc
-echo "source ${DIRNAME}/zsh/.zshrc" >> .zshrc
-success "oh-my-zsh configured = .zshrc"
-cat ${HOME}.zshrc
-echo "-----"
+step "Configure oh-my-zsh"
+if [ -z $(command -v ascii-dunno) ]; then
+  echo "## Custom config from: https://github.com/smnielsen/config" >> .zshrc
+  echo "export SMN_CONFIG_DIR=${DIRNAME}" >> .zshrc
+  echo "source ${DIRNAME}/zsh/.zshrc" >> .zshrc
+  success "oh-my-zsh configured = .zshrc"
+else
+  success "ZSH already configured"
+fi
 
 ###############################
 ## SETUP GIT CONFIG
-log "GIT - Global Config"
+step "Git Configuration"
 git config --global user.name "Simon Nielsen"
 git config --global user.email "simonmtnielsen@gmail.com"
 git config --global core.ignorecase false
+success "Configured GIT"
 
 ###############################
 # SSH Creation and add
@@ -149,28 +168,57 @@ if [ ! -f "${HOME}/.ssh/id_rsa.pub" ]; then
 fi
 
 ###############################
+# Homebrew
+step "Install Homebrew"
+if [ -z $(command -v brew) ]; then
+  log "Installing Homebrew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  success "Installed Homebrew"
+else
+  success "Homebrew already installed"
+fi
+
+###############################
+# Setup dev dirs
+mkdir -p ${HOME}/dev
+mkdir -p ${HOME}/dev/private
+
+###############################
 ## INSTALL ALL APPLICATIONS
-log "=== Homebrew - Terminal Apps ==="
+echo ""
+log "All configuration steps done."
+step "Install All Applications"
+while true; do
+  read -p "Do you wish to continue (y/n)? " yn
+  case $yn in
+    [Yy]* ) echo "Continuing with installation..."; break;;
+    [Nn]* ) echo "Exiting installation!"; exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+
+
+step "=== Homebrew - Terminal Apps ==="
 for program in "${BREW[@]}"; do
   log "Installing $program..."
   brew install $program >> /dev/null
-  success "$program installed"
 done
+success "Terminal apps installed"
 echo ""
 
-log "Installing zgen"
+step "Installing zgen"
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 success "Installed zgen"
 
-log "=== Homebrew Cask - Programs ==="
+step "=== Homebrew Cask - Programs ==="
 for program in "${CASK[@]}"; do
   log "Installing $program..."
   brew cask $program >> /dev/null
-  success "$program installed"
 done
+success "Cask applications installed"
 echo ""
 
-log "=== Mac App Store - Applications ==="
+step "=== Mac App Store - Applications ==="
 for program in "${MAS[@]}"; do
   split=(${program//:/ })
   name=${split[0]}
@@ -178,37 +226,30 @@ for program in "${MAS[@]}"; do
 
   log "max install $name: $id"
   mas install $id >> /dev/null
-  success "Installed $name: $id"
 done
+success "Mac App Store applications installed"
 echo ""
 
-log "=== Yarn - Global Apps ==="
+step "=== Yarn - Global Apps ==="
 for program in "${YARN_GLOBAL[@]}"; do
   log "Yarn install $program..."
   yarn global add $program
-  success "$program installed"
 done
+success "Yarn/Npm apps installed"
 echo ""
 
 ###############################
 # Load .zshrc
-source ${HOME}.zshrc
+source ${HOME}/.zshrc
 
 ###############################
-# Setup dev dirs
-mkdir -p ${HOME}/dev
-mkdir -p ${HOME}/dev/go/src/github.com/smnielsen
-mkdir -p ${HOME}/dev/private
-mkdir -p ${HOME}/dev/netlight
-
-###############################
-echo ">>>> Finilized setting up Mac <<<<"
-echo "   Welcome to a new world Simon   "
+step ">>>> Finilized setting up Mac <<<<"
+log "   Welcome to a new world!   "
 echo ""
-echo "## Installed:"
+log "## Installed:"
 echoAll
 echo ""
-echo "## Requires Manual installation:"
+step "## Requires Manual installation:"
 for program in "${MANUAL[@]}"; do
   split=(${program//;/ })
   name=${split[0]}
@@ -217,14 +258,15 @@ for program in "${MANUAL[@]}"; do
   echo ""
 done
 echo ""
-echo "-- Add Custom Colors to terminal"
-echo "   > iterm2 -> Preferences -> Profiles -> Colors -> Color Presets... -> Import"
-echo "   > Choose iterm/netlight-colors.itermcolors"
-echo "   > Color presets... -> netlight-colors"
-echo "-- Set to always open previous dir"
-echo "   > iterm2 -> Preferences -> Profiles -> General -> Working directory -> Reuse previous ..."
+log "-- Add Custom Colors to terminal"
+log "   > iterm2 -> Preferences -> Profiles -> Colors -> Color Presets... -> Import"
+log "   > Choose iterm/netlight-colors.itermcolors"
+log "   > Color presets... -> netlight-colors"
+log "-- Set to always open previous dir"
+log "   > iterm2 -> Preferences -> Profiles -> General -> Working directory -> Reuse previous ..."
 echo "--------------------"
-echo "FYI: You should probly restart your terminal now..."
+success "FYI: You should probly restart your terminal now..."
+ascii-dunno
 echo ""
 
 cd ${PREV_DIR}
